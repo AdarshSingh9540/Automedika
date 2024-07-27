4f2cd0825758f0a94c16612df8d7788544fbf8bb
Adarsh <adarsh9540984202@gmail.com>
2024-07-27 18:36:17 +0530

    remove searchbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import 'regenerator-runtime/runtime'; // Add this line at the very top
 import Bar from "./components/Bar"
 import AppBar from './components/AppBar';
-import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <>
       <AppBar />
-      {/* <SearchBar /> */}
+
       <Bar/>
     </>
   );

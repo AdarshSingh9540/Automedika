c566512dde91e1985a4184dedd8d015ebe322128
Adarsh <adarsh9540984202@gmail.com>
2024-07-27 18:45:32 +0530

    search bar is added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import 'regenerator-runtime/runtime'; // Add this line at the very top
-import Bar from "./components/Bar"
+
 import AppBar from './components/AppBar';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <>
       <AppBar />
-      <Bar/>
+      <SearchBar/>
+  
     </>
   );
 }
